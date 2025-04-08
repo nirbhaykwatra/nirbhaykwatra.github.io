5972665cdba8dee06294dadb68fce173ee555d2b
Nirbhay Kwatra <kwatranirbhay@gmail.com>
2025-04-08 09:47:42 -0700

    Add school list to markers

--- assets/index.js
+++ assets/index.js
@@ -2,19 +2,19 @@ async function initMap() {
     // Request needed libraries.
     const { Map } = await google.maps.importLibrary("maps");
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-    const center = { lat: 37.43238031167444, lng: -122.16795397128632 };
+    const center = { lat: 49.3292264, lng: -123.0726468 };
     const map = new Map(document.getElementById("map"), {
       zoom: 11,
       center,
       mapId: "4504f8b37365c3d0",
     });
   
-    for (const property of properties) {
+    for (const school of schools) {
       const AdvancedMarkerElement = new google.maps.marker.AdvancedMarkerElement({
         map,
-        content: buildContent(property),
-        position: property.position,
-        title: property.description,
+        content: buildContent(school),
+        position: school.position,
+        title: school.name,
       });
   
       AdvancedMarkerElement.addListener("gmp-click", () => {
@@ -36,31 +36,20 @@ async function initMap() {
   function buildContent(property) {
     const content = document.createElement("div");
   
-    content.classList.add("property");
+    content.classList.add("school");
     content.innerHTML = `
       <div class="icon">
           <i aria-hidden="true" class="fa fa-icon fa-${property.type}" title="${property.type}"></i>
           <span class="fa-sr-only">${property.type}</span>
       </div>
       <div class="details">
-          <div class="price">${property.price}</div>
+          <div class="name">${property.name}</div>
           <div class="address">${property.address}</div>
-          <div class="description">${property.description}</div>
-          <div class="features">
+          <div class="district">${property.district}</div>
+          <div class="requests">
+          <p>Requests</p>
           <div>
-              <i aria-hidden="true" class="fa fa-bed fa-lg bed" title="bedroom"></i>
-              <span class="fa-sr-only">bedroom</span>
-              <span>${property.bed}</span>
-          </div>
-          <div>
-              <i aria-hidden="true" class="fa fa-bath fa-lg bath" title="bathroom"></i>
-              <span class="fa-sr-only">bathroom</span>
-              <span>${property.bath}</span>
-          </div>
-          <div>
-              <i aria-hidden="true" class="fa fa-ruler fa-lg size" title="size"></i>
-              <span class="fa-sr-only">size</span>
-              <span>${property.size} ft<sup>2</sup></span>
+              <span>${property.requests[0]}</span>
           </div>
           </div>
       </div>
@@ -200,5 +189,56 @@ async function initMap() {
       },
     },
   ];
+
+  const schools = [
+    {
+      name: "Capilano Elementary School",
+      type: "school",
+      address: "1230 20th St W, North Vancouver, BC",
+      district: "School District 44",
+      requests: ["Request 1"],
+      programs: ["Program 1"],
+      position: {
+        lat: 49.3280176,
+        lng: -123.1087725
+      }
+    },
+    {
+      name: "Highlands Elementary School",
+      type: "school",
+      address: "3150 Colwood Dr, North Vancouver, BC",
+      district: "School District 44",
+      requests: ["Request 1"],
+      programs: ["Program 1"],
+      position: {
+        lat: 49.3384867,
+        lng: -123.0975259
+      }
+    },
+    {
+      name: "Larson Elementary School",
+      type: "school",
+      address: "2605 Larson Rd, North Vancouver, BC",
+      district: "School District 44",
+      requests: ["Request 1"],
+      programs: ["Program 1"],
+      position: {
+        lat: 49.3333691,
+        lng: -123.085248
+      }
+    },
+    {
+      name: "Westview Elementary School",
+      type: "school",
+      address: "641 17th Street West, North Vancouver, BC",
+      district: "School District 44",
+      requests: ["Request 1"],
+      programs: ["Program 1"],
+      position: {
+        lat: 49.3238414,
+        lng: -123.0894738
+      }
+    }
+  ]
   
   initMap();
